076dafd0c122a003f436646a2ef5a87318166352
charleypeng <charleypeng@gmail.com>
2026-07-24 22:12:21 +0800

    rename app to Kakabin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -156,5 +156,5 @@ pub fn run() {
             }
         })
         .run(tauri::generate_context!())
-        .expect("error while running BlkDustBin");
+        .expect("error while running Kakabin");
 }
